f8dc802774328476431d79376d8f33ab1dfc07f4
innocent-abhishek <01fe24bcs122@kletech.ac.in>
2026-07-20 17:37:13 +0530

    add sub array practice 1

---
+++ subarray_create.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+  vector<int> nums={1,2,3,4};
+   int n=nums.size();
+
+  for(int i=0;i<n;i++)
+  {
+    cout<<nums[i]<<" ";
+  }
+
+
+}
