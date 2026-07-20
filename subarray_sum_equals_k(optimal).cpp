0d9d64390b40b2832e8ded4506d38edff7ef8ab3
innocent-abhishek <01fe24bcs122@kletech.ac.in>
2026-07-20 22:20:06 +0530

    Add optimal solution for Subarray Sum Equals K using prefix sum and hash map

---
+++ subarray_sum_equals_k(optimal).cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+     unordered_map<int,int>mp;
+     int n=nums.size();
+     int result=0;
+     int sum=0;
+     mp.insert({0,1});
+     for(int i=0;i<n;i++)
+      { sum=sum+nums[i];
+      if(mp.find(sum-k)!=mp.end())
+      {
+        result+=mp[sum-k];
+     
+      }
+
+        mp[sum]++;
+
+
+      }
+
+     
+
+      return result;
+
+
+    }
+};
