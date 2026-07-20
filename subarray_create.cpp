eabed9d9ea3fc6656284f8f7ecc7b344385678c5
innocent-abhishek <01fe24bcs122@kletech.ac.in>
2026-07-20 18:21:20 +0530

    modified fiel

--- subarray_create.cpp
+++ subarray_create.cpp
@@ -8,9 +8,9 @@ int main(){
 
   for(int i=0;i<n;i++)
   {
-    for(int j=0;j<n;j++)
+    for(int j=i;j<n;j++)
     {
-         cout<<nums[i]<<" ";
+         cout<<"("<<i<<","<<j<<")";
     }
     cout<<endl;
 
@@ -18,3 +18,8 @@ int main(){
 
 
 }
+/*out put
+(0,0)(0,1)(0,2)(0,3)
+(1,1)(1,2)(1,3)
+(2,2)(2,3)
+(3,3)*/
