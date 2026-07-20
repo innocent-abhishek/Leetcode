9023b71e9453234f5c8c39f1620f2ce33a016a95
innocent-abhishek <01fe24bcs122@kletech.ac.in>
2026-07-20 18:31:08 +0530

    add  leetcode 560

---
+++ subarray_sum_equals_k.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        int sum=0;
+        int ans=0;
+        for(int i=0;i<n;i++){
+            sum=0;
+            for(int j=i;j<n;j++)
+            {
+               
+                sum=sum+nums[j];
+                if(sum==k){
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
