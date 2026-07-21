fc05edfdc1adb926dd87d4ffbb286f41278632a2
innocent-abhishek <01fe24bcs122@kletech.ac.in>
2026-07-21 15:08:28 +0530

    add move zeros to the end

---
+++ move_zeros.cpp
@@ -0,0 +1,22 @@
+/*if the number is not zero 
+then swap th
+*/
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+    
+        int k=0;
+        int n=nums.size();
+       for(int i=0;i<n;i++)
+       {  if(nums[i]!=0)
+       {
+        swap(nums[i],nums[k]);
+        k++;
+       }
+
+
+
+       }
+    }
+};
